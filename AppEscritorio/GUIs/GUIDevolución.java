9e96e4b6a3886d2eb21d5327dbe983ce29eec92d
Abner Arcia <at.abner.e@gmail.com>
2019-04-27 19:47:24 -0500

    Ya funciona la devolucion, el cambio no del todo. La funcion de reporte muestra un dialogo que pide el ID de un vuelo y genera un PDF de los boletos vendidos

--- AppEscritorio/GUIs/GUIDevolución.java
+++ AppEscritorio/GUIs/GUIDevolución.java
@@ -4,15 +4,32 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import Otros.Conexion;
 import net.miginfocom.swing.MigLayout;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class GUIDevolución 
 {
@@ -47,12 +64,99 @@ public class GUIDevolución
 	btnCambio.setCursor                (Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	btnCambio.setPreferredSize(new Dimension(100,50));
 	btnCambio.setBackground(Color.WHITE);
+	//Agregar el ActionListener al boton Cambio
+	btnCambio.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			String idBoleto = txtBoleto.getText();
+			Date fechaActual = new Date();
+			java.sql.Date sqlFechaActual = new java.sql.Date(fechaActual.getTime());
+			String fecha = sqlFechaActual.toString();
+			String query = "SELECT b.status FROM boletos b " + 
+					"INNER JOIN vuelos v " + 
+					"ON b.vuelos_idVuelo = v.idVuelo " + 
+					"WHERE idBoleto = '" + idBoleto + "' AND '" + fecha + "' <= DATE_SUB(fecha, INTERVAL 2 DAY)";
+			Conexion con = new Conexion();
+			try {
+				ResultSet status = con.obtenerDatos(query);
+				boolean valido = status.isBeforeFirst();
+				if (valido) {
+					status.next();
+					String valor = status.getString(1);
+					if (valor.equals("0")) {
+						//Hacer el cambio del boleto
+						JOptionPane.showMessageDialog(null, "Boleto correcto: Cambiar boleto", "Cambiar boleto", JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						JOptionPane.showMessageDialog(null, "El boleto ya ha sido cancelado o ya se ha confirmado previamente "
+								+ "y ya no es posible cambiarlo", 
+								"Cambiar boleto", JOptionPane.INFORMATION_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "No existe el boleto o ya no se encuentra en el periodo de cambio", 
+							"Cambiar boleto", JOptionPane.INFORMATION_MESSAGE);
+				}
+				con.cerrarConexion();
+			} catch (SQLException ex) {					
+				ex.printStackTrace();
+			}
+		}
+	});
 	
 	btnDevolucion.setBorder                (null )     ;
 	btnDevolucion.setOpaque                (false)     ;
 	btnDevolucion.setCursor                (Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	btnDevolucion.setPreferredSize(new Dimension(100,50));
 	btnDevolucion.setBackground(Color.WHITE);
+	//Agregar el ActionListener al boton Devolucion
+	btnDevolucion.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			String idBoleto = txtBoleto.getText();
+			Date fechaActual = new Date();
+			java.sql.Date sqlFechaActual = new java.sql.Date(fechaActual.getTime());
+			String fecha = sqlFechaActual.toString();
+			String query = "SELECT b.status FROM boletos b " + 
+					"INNER JOIN vuelos v " + 
+					"ON b.vuelos_idVuelo = v.idVuelo " + 
+					"WHERE idBoleto = '" + idBoleto + "' AND '" + fecha + "' <= DATE_SUB(fecha, INTERVAL 2 DAY)";
+			Conexion con = new Conexion();
+			try {
+				ResultSet status = con.obtenerDatos(query);
+				boolean valido = status.isBeforeFirst();
+				if (valido) {
+					status.next();
+					String valor = status.getString(1);
+					if (valor.equals("0")) {
+						query = "UPDATE boletos b SET b.status = 2 WHERE idBoleto = '" + idBoleto + "'";
+						Connection conn = new Conexion().getConnection();
+						conn.createStatement().execute(query);
+						//Crear el objeto JasperReport cargando el archivo de reporte .jasper
+						JasperReport reporte = (JasperReport) JRLoader.loadObject(GUICheckIn.class.getResource("/Otros/ReciboDevolucion.jasper"));
+						//Crear un HashMap para ingresar el parametro ID de boleto
+						HashMap<String, Object> parametro = new HashMap<String, Object>();
+						parametro.put("ID", idBoleto);
+						//Crear el objeto JasperPrint que basicamente ya es el pase para abordar, dandole el reporte, el parametro y una conexion a BD
+						JasperPrint doc = JasperFillManager.fillReport(reporte, parametro, conn);
+						//Crear un JasperViewer para ver el pdf, pero el JasperViewer no es modal asi que se adapta a un JDialog
+						JasperViewer jv = new JasperViewer(doc, false);
+						JDialog dialog = new JDialog((JFrame)null, true);
+						dialog.setContentPane(jv.getContentPane());
+						dialog.setSize(jv.getSize());
+						dialog.setTitle("Recibo de devoluci\u00F3n");
+						dialog.setVisible(true);
+					} else {
+						JOptionPane.showMessageDialog(null, "El boleto ya ha sido cancelado o ya se ha confirmado previamente "
+								+ "y ya no es posible cancelarlo", 
+								"Devolver boleto", JOptionPane.INFORMATION_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "No existe el boleto o ya no se encuentra en el periodo de devoluci\u00F3n", 
+							"Devolver boleto", JOptionPane.INFORMATION_MESSAGE);
+				}
+				con.cerrarConexion();
+			} catch (JRException | SQLException ex) {					
+				ex.printStackTrace();
+			}
+		}
+	});
 	
 	lblTexto.setOpaque    (true);
 	lblTexto.setBackground(new Color(0,102,177));
